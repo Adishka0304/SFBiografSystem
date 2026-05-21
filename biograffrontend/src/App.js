410ab47bb678c1e2bb3d8e8861d7c7b79a45e6e5
Adishka0304 <Adishka.tarin@gmail.com>
2026-05-21 23:20:29 +0200

    Ta bort onödiga mappar

--- biograffrontend/src/App.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import { MoviePage } from "./pages/MoviePage";
-import "./App.css";
-function App() { return <MoviePage />; }
-export default App;
--- biograffrontend/src/components/SearchBar.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-export function SearchBar({ value, onChange }) {
-  return (
-    <div className="search">
-      <input className="search__input" type="text" placeholder="Sok..." value={value} onChange={e => onChange(e.target.value)} />
-      {value && <button className="search__clear" onClick={() => onChange("")}>X</button>}
-    </div>
-  );
-}
